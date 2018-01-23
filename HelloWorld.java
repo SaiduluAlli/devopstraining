2fbe1fa0f8b0664db28b266de5ec8881888ff689
Saidulu Alli <saidulu.alli@gmail.com>
2018-01-23 03:05:10 +0000

    git push demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public static void main(String args[]){
 System.out.println("Welcome to java!");
 System.out.println("From Master branch!");
 System.out.println("From branch testing!");
+// git push demo
+System.out.println("Git push demo!");
 }
 }
